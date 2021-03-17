276993bd631650978bb52f51723036e53d704ced
Scarlet-Xu <yuyanran.20@intl.zju.edu.cn>
2021-03-17 09:51:59 +0800

    Coronavirus_rates.py List_manipulation.py

--- Practical6/Coronavirus_rates.py
+++ Practical6/Coronavirus_rates.py
@@ -7,9 +7,9 @@ countries = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':43608
 # use matplotlib to draw a statistical chart
 import matplotlib.pyplot as plt
 # make a list of countries
-labels = 'USA','India','Brazil','Russia','UK'
+labels = countries.keys()
 # input the total cases of each country
-sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
+sizes =  countries.values()
 # show the distance each part away from the center of the circle
 explode = (0, 0.1, 0, 0,0)
 # 
